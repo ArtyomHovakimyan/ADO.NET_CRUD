b6e532c9aaaaf429f7b691f1ab5e1d94f1b62270
User <artyom.hovakimyan01@gmail.com>
2019-04-20 20:13:34 +0400

    add insert sql

--- Mic.Volo.AdoNetEx/Program.cs
+++ Mic.Volo.AdoNetEx/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace Mic.Volo.AdoNetEx
 {
@@ -10,6 +12,19 @@ namespace Mic.Volo.AdoNetEx
     {
         static void Main(string[] args)
         {
+            string connectoinString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            string sqlExpression = "Insert into Users(Name,Age) Values('Name1',21)";
+            using (SqlConnection connection = new SqlConnection(connectoinString))
+            {
+                connection.Open();
+                Console.WriteLine("connected");
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                int number = command.ExecuteNonQuery();
+                Console.WriteLine("added objects: {0}", number);
+
+            }
+
+            Console.WriteLine("Close connect...");
         }
     }
 }
